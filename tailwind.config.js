76ea83411e7e39b2d33b8d80c8577c8dd90a2910
Dimitris <dimitris@techangel.gr>
2021-01-23 21:52:11 +0200

    head working css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: {
-    content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html"],
+    content: [
+        "./layouts/**/*.html", 
+        "./content/**/*.md", 
+        "./content/**/*.html"
+    ],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
