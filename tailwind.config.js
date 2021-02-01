bac8082fd208a074c187f4b937192877017dbe84
Dimitris <dimitris@techangel.gr>
2021-02-01 12:42:19 +0200

    fonts not working yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 module.exports = {
   purge: {
     content: [
-        "./layouts/**/*.html", 
-        "./content/**/*.md", 
-        "./content/**/*.html"
+      "./layouts/**/*.html",
+      "./content/**/*.md",
+      "./content/**/*.html",
     ],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        robregular: ['Roboto'],
+        robmono: ['Roboto Mono'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
