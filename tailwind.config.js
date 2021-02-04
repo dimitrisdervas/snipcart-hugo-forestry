6137dea7b8278847c4fea4edef7a98163970322d
dimitrisdervas <dimitris@techangel.gr>
2021-02-04 09:05:15 +0200

    changed to bep repositroy for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,66 @@
+const theme = require('tailwindcss/defaultTheme');
+const typography = require('@tailwindcss/typography');
+
+//const colorBrand = 'var(--color-pretty)';
+
+// Utils
+const round = (num) => num.toFixed(7).replace(/(\.[0-9]+?)0+$/, '$1').replace(/\.0$/, '');
+const rem = (px) => `${round(px / 16)}rem`;
+const em = (px, base) => `${round(px / base)}em`;
+const px = (px) => `${px}px`;
+
 module.exports = {
-  purge: {
+	important: true, // See https://tailwindcss.com/docs/configuration#important
+	purge: {
+		enabled: process.env.HUGO_ENVIRONMENT === 'production',
     content: [
-      "./layouts/**/*.html",
-      "./content/**/*.md",
-      "./content/**/*.html",
-    ],
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        robregular: ['Roboto'],
-        robmono: ['Roboto Mono'],
+      './hugo_stats.json',
+      './layouts/**/*.html',
+		],
+		extractors: [
+      {
+        extractor: (content) => {
+					let els = JSON.parse(content).htmlElements;
+					return els.tags.concat(els.classes, els.ids);
+				},
+        extensions: ['json']
       },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    ],
+		mode: 'all',
+		
+	},
+	theme: {
+	  extend: {
+		fontFamily: {
+		  robregular: ['Roboto'],
+		  robmono: ['Roboto Mono'],
+		},
+	  },
+	},
+	plugins: [ typography ]
 };
+
+// https://github.com/bep/hugo-starter-tailwind-basic/blob/master/tailwind.config.js
+
+// module.exports = {
+// 	purge: {
+// 	  content: [
+// 		"./layouts/**/*.html",
+// 		"./content/**/*.md",
+// 		"./content/**/*.html",
+// 	  ],
+// 	},
+// 	darkMode: false, // or 'media' or 'class'
+// 	theme: {
+// 	  extend: {
+// 		fontFamily: {
+// 		  robregular: ['Roboto'],
+// 		  robmono: ['Roboto Mono'],
+// 		},
+// 	  },
+// 	},
+// 	variants: {
+// 	  extend: {},
+// 	},
+// 	plugins: [],
+//   };
