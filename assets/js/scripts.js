62aee5ba3ff6f0883999774c5d2e89ce07ff3212
Dimitris <dimitris@techangel.gr>
2021-02-11 20:08:50 +0200

    fixed import of node modules js

---
+++ assets/js/scripts.js
@@ -0,0 +1,54 @@
+import Swiper from "swiper";
+import Alpine from "alpinejs";
+import Lazysizes from "lazysizes";
+
+
+{{/*  https://github.com/nolimits4web/swiper/blob/master/demos/320-multiple-swipers.html  */}}
+        var swiper = new Swiper('.heroslideshow', {
+            loop: 'true',
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'progressbar',
+                dynamicBullets: true,
+            },
+        });
+
+
+        var swiper = new Swiper('.productspromoted', {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            loop: 'true',
+            navigation: {
+                nextEl: '.productspromoted-next',
+                prevEl: '.productspromoted-prev',
+            },
+            breakpoints: {
+                320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+                },
+                // when window width is >= 480px
+                480: {
+                slidesPerView: 2,
+                spaceBetween: 30
+                },
+                640: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+                },
+                768: {
+                slidesPerView: 4,
+                spaceBetween: 10,
+                }
+            },
+        });
+
+    if (window.netlifyIdentity) {
+    window.netlifyIdentity.on('init', (user) => {
+        if (!user) {
+        window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+        });
+        }
+    });
+    }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,57 +10,30 @@ const em = (px, base) => `${round(px / base)}em`;
 const px = (px) => `${px}px`;
 
 module.exports = {
-	important: true, // See https://tailwindcss.com/docs/configuration#important
-	purge: {
-		enabled: process.env.HUGO_ENVIRONMENT === 'production',
-    content: [
-      './hugo_stats.json',
-      './layouts/**/*.html',
-		],
-		extractors: [
+  // See https://tailwindcss.com/docs/configuration#important
+  purge: {
+    enabled: process.env.HUGO_ENVIRONMENT === "production",
+    content: ["./hugo_stats.json", "./layouts/**/*.html"],
+    extractors: [
       {
         extractor: (content) => {
-					let els = JSON.parse(content).htmlElements;
-					return els.tags.concat(els.classes, els.ids);
-				},
-        extensions: ['json']
+          let els = JSON.parse(content).htmlElements;
+          return els.tags.concat(els.classes, els.ids);
+        },
+        extensions: ["json"],
       },
     ],
-		mode: 'all',
-		
-	},
-	theme: {
-	  extend: {
-		fontFamily: {
-		  robregular: ['Roboto'],
-		  robmono: ['Roboto Mono'],
-		},
-	  },
-	},
-	plugins: [ typography ]
+    mode: "all",
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        robregular: ["Roboto-Regular"],
+        robmono: ["Roboto-mono"],
+      },
+    },
+  },
+  plugins: [typography],
 };
 
 // https://github.com/bep/hugo-starter-tailwind-basic/blob/master/tailwind.config.js
-
-// module.exports = {
-// 	purge: {
-// 	  content: [
-// 		"./layouts/**/*.html",
-// 		"./content/**/*.md",
-// 		"./content/**/*.html",
-// 	  ],
-// 	},
-// 	darkMode: false, // or 'media' or 'class'
-// 	theme: {
-// 	  extend: {
-// 		fontFamily: {
-// 		  robregular: ['Roboto'],
-// 		  robmono: ['Roboto Mono'],
-// 		},
-// 	  },
-// 	},
-// 	variants: {
-// 	  extend: {},
-// 	},
-// 	plugins: [],
-//   };
