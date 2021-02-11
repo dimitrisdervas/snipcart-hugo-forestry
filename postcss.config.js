cad0903c82aab13fe21ad0de5b5e626bebee8ead
Dimitris <dimitris@techangel.gr>
2021-02-11 20:27:24 +0200

    imported swiper to main css

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,15 @@
 let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
 const tailwind = require('tailwindcss')(tailwindConfig);
 const autoprefixer = require('autoprefixer');
+const easyimport = require('postcss-easy-import');
 
 module.exports = {
-	// eslint-disable-next-line no-process-env
-	plugins: [ tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ autoprefixer ] : []) ]
+  // eslint-disable-next-line no-process-env
+  plugins: [
+        easyimport,
+        tailwind,
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [autoprefixer] : []),
+  ],
 };
 
 // https://github.com/bep/hugo-starter-tailwind-basic/blob/master/postcss.config.js
